3af323df3db7985318fffb86a90e718804ade038
houtian <44050233+houtianzzj@users.noreply.github.com>
2020-04-10 14:09:01 +0800

    20200410

---
+++ template/header.php
@@ -0,0 +1,11 @@
+<div class="bg-primary position-relative">
+  <div class="overlay-gradient"></div>
+  <div class="container position-relative">
+  	<div class="row py-5">
+    	<div class="col">
+        <h1 class="display-1">每日一学网</h1>
+        <p><a href="http://www.meiriyixue.cn" class="btn btn-primary btn-lg shadow" role="button">了解更多 &raquo;</a></p>
+    </div>
+    </div>
+  </div>
+</div>
